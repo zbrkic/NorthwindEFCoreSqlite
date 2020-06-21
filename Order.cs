981346217aecdeee13bace8b0abda64fde675ff1
Željko Brkić <zeljko.brkic@gmail.com>
2020-06-21 23:19:06 +0200

    Add project files.

---
+++ Order.cs
@@ -0,0 +1,20 @@
+﻿namespace NorthwindEFCoreSqlite
+{
+    public partial class Order
+    {
+        public long OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public long? EmployeeId { get; set; }
+        public byte[] OrderDate { get; set; }
+        public byte[] RequiredDate { get; set; }
+        public byte[] ShippedDate { get; set; }
+        public long? ShipVia { get; set; }
+        public double? Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+    }
+}
